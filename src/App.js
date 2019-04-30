d8bc9d719fe9430c13dabf2b267dcceefadcbef2
lizkoch <46996982+lizkoch@users.noreply.github.com>
2019-04-30 21:19:36 +0100

    Made first changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is my super cool weather app and when I get home I'll work on it!
         </p>
         <a
           className="App-link"
