3956a66526154287c3de03f469d27be1edf0e3d5
lizkoch <46996982+lizkoch@users.noreply.github.com>
2019-05-07 17:26:25 +0100

    Added html and css

--- src/App.js
+++ src/App.js
@@ -4,20 +4,67 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          This is my super cool weather app and when I get home I'll work on it!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div classNameName="App">
+      <header classNameName="App-header">
+        <h1>Where is it sunny?</h1>
+
+        <div className="container">
+          <div className="weather">
+            <div className="main-weather">
+              <form className="clearfix float-left" id="weather-search-form">
+                <div className="form-group float-left">
+                  <input type="text" placeholder="Which city is calling you?" autocomplete="off" className="form-control"
+                    id="weather-search-text-input" />
+                </div>
+                <input type="submit" value="Search" className="btn btn-info float-left" />
+              </form>
+              <button id="current-location-button" className="btn btn-primary float-left">
+                Where You Are
+                </button>
+            </div>
+            <br />
+            <br />
+            <h1 id="weather-city"></h1>
+            <h2 className="float-left">
+              <span id="weather-temperature"></span><small>°C</small>
+            </h2>
+            <br />
+            <br />
+            <ul>
+              <li id="weather-timestamp"></li>
+              <li id="weather-description"></li>
+              <li>Humidity: <span id="weather-humidity-level"></span>%</li>
+            </ul>
+            <div className="row">
+              <div className="col-md-6">
+                <div className="clearfix">
+
+                  <br />
+                  <br />
+                  <h2>Need Inspiration?</h2>
+                  <div className="sunnyCities row">
+                    <div className="cityBox1 col-sm-4">
+                      <h3>Lisbon</h3>
+                      <h4>Portugal</h4>
+                      <p>12 hours of sun</p>
+                      <img src="" className="imgBox" />
+                      <p>18ºC</p>
+                    </div>
+                    <div className="cityBox2 col-sm-4">
+                      <h3>Cape Town</h3>
+                      <h4>South Africa</h4>
+                      <p>12 hours of sun</p>
+                      <img src="" className="imgBox" />
+                      <p>18ºC</p>
+                    </div>
+                    <div className="cityBox3 col-sm-4">
+                      <h3>Sydney</h3>
+                      <h4>Australia</h4>
+                      <p>12 hours of sun</p>
+                      <img src="" className="imgBox" />
+                      <p>18ºC</p>
+                    </div>
+                  </div>
       </header>
     </div>
   );
