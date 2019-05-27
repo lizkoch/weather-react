9318dfd0fc728eb8d0ae69622e919ebf1e6b6069
lizkoch <46996982+lizkoch@users.noreply.github.com>
2019-05-27 22:17:18 +0100

    Testing edits on search functionality

--- src/Main.js
+++ src/Main.js
@@ -21,34 +21,55 @@ export default class Main extends Component {
     );
     const response = await api_call.json();
     console.log(response);
-    if (city) {
-      this.setState({
-        temperature: response.main.temp,
-        city: response.name,
-        humidity: response.main.humidity,
-        description: response.weather[0].description,
-        sunrise: response.data.sys.sunrise,
-        sunset: response.data.sys.sunset,
-        icon: response.data.weather[0].icon,
-        error: ""
-      });
-    } else {
-      this.setState({
-        error: "Please input a city..."
-      });
-    }
+
+    this.setState({
+      loaded: true,
+      temperature: response.main.temp,
+      city: response.name,
+      humidity: response.main.humidity,
+      description: response.weather[0].description,
+      sunrise: response.data.sys.sunrise,
+      sunset: response.data.sys.sunset,
+      icon: response.data.weather[0].icon,
+      error: ""
+    });
   };
 
   render() {
-    return (
-      <div className="main-weather">
-        <Search loadWeather={this.getWeather} />
-        <h2>{this.state.city}</h2>
-        <p className="main-description">{this.state.description}</p>
-        <p className="main-humidity">Chance of rain: {this.state.humidity}</p>
-        <p className="main-sunrise">Sunrise: {this.state.sunrise}</p>
-        <p className="main-sunset">Sunset: {this.state.sunset}</p>
-      </div>
-    );
+    if (this.state.loaded) {
+      return (
+        <div className="main-weather">
+          <Search loadWeather={this.getWeather} />
+          <h2>{this.state.city}</h2>
+          <p className="main-description">{this.state.description}</p>
+          <p className="main-humidity">Chance of rain: {this.state.humidity}</p>
+          <p className="main-sunrise">Sunrise: {this.state.sunrise}</p>
+          <p className="main-sunset">Sunset: {this.state.sunset}</p>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <form onSubmit={this.props.loadWeather}>
+            <div className="form-group float-left">
+              <input
+                type="text"
+                placeholder="Which city is calling you?"
+                autoComplete="off"
+                className="form-control"
+                id="weather-search-text-input"
+              />
+            </div>
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-info float-left"
+            />
+          </form>
+          <br />
+          <br />
+        </div>
+      );
+    }
   }
 }
