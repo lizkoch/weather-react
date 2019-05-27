ac4f7341f3635ca67e69d09b3a8d247580b10457
lizkoch <46996982+lizkoch@users.noreply.github.com>
2019-05-27 16:32:38 +0100

    Got rid of error

--- src/Main.js
+++ src/Main.js
@@ -15,18 +15,16 @@ export default class Main extends Component {
 
   getWeather = async e => {
     const city = e.target.elements.city.value;
-    const country = e.target.elements.country.value;
     e.preventDefault();
     const api_call = await fetch(
       `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${Api_Key}`
     );
     const response = await api_call.json();
     console.log(response);
-    if (city && country) {
+    if (city) {
       this.setState({
         temperature: response.main.temp,
         city: response.name,
-        country: response.sys.country,
         humidity: response.main.humidity,
         description: response.weather[0].description,
         sunrise: response.data.sys.sunrise,
