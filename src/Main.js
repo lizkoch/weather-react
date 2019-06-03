69eca788bcab1f41eaddb21c8e2919c5b2cc83fe
lizkoch <46996982+lizkoch@users.noreply.github.com>
2019-06-03 17:05:25 -0400

    Icon next to weather

--- src/Main.js
+++ src/Main.js
@@ -8,32 +8,38 @@ class Main extends Component {
   render() {
     return (
       <div className="weather-info">
-        {this.props.country && this.props.city && (
-          <p className="weather-key">
-            Location:
-            <span className="weather-value">
-              {" "}
-              {this.props.city}, {this.props.country}
-            </span>
-          </p>
-        )}
-        {this.props.temperature && (
-          <p className="weather-key">
-            Temperature:
-            <span className="weather-value"> {this.props.temperature}°C</span>
-          </p>
-        )}
-        {this.props.description && (
-          <p className="weather-key">
-            Conditions:
-            <span className="weather-value"> {this.props.description}</span>
-          </p>
-        )}
-        {this.props.icon && (
-          <p className="icon">
-            <Icons icon={this.props.icon} size={200} color="#ffc107" />
-          </p>
-        )}
+        <div className="column">
+          {this.props.icon && (
+            <p className="icon">
+              <Icons icon={this.props.icon} size={200} color="#ffc107" />
+            </p>
+          )}
+        </div>
+        <br />
+        <div className="column">
+          {this.props.country && this.props.city && (
+            <p className="weather-key">
+              Location:
+              <span className="weather-value">
+                {" "}
+                {this.props.city}, {this.props.country}
+              </span>
+            </p>
+          )}
+          {this.props.temperature && (
+            <p className="weather-key">
+              Temperature:
+              <span className="weather-value"> {this.props.temperature}°C</span>
+            </p>
+          )}
+          {this.props.description && (
+            <p className="weather-key">
+              Conditions:
+              <span className="weather-value"> {this.props.description}</span>
+            </p>
+          )}
+        </div>
+
         {this.props.error && (
           <p className="weather-error">{this.props.error}</p>
         )}
